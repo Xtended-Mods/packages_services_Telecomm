930b7458c44971c2af3e23e3e0088ca80d7fd40e
Shripal17 <shripal17@gmail.com>
2018-06-05 15:54:20 +0000

    Telecomm: Add Flashlight blink on incoming calls [2/3]
    - Add 4 different cases disabled, ringer, dnd mode and always on.
    - Add a delay of 500ms for making it blink faster
    
    - Thanks to @PMS22 for helping
    
    Change-Id: Iec1ea643f01cb5640a4f61203520bbd63b2810bd
    Signed-off-by: PMS22 <prathams99@rediff.com>

--- src/com/android/server/telecom/Ringer.java
+++ src/com/android/server/telecom/Ringer.java
@@ -20,6 +20,9 @@ import static android.provider.CallLog.Calls.USER_MISSED_DND_MODE;
 import static android.provider.CallLog.Calls.USER_MISSED_LOW_RING_VOLUME;
 import static android.provider.CallLog.Calls.USER_MISSED_NO_VIBRATE;
 
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraManager;
+import android.os.AsyncTask;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.Person;
@@ -213,6 +216,8 @@ public class Ringer {
 
     private CompletableFuture<Void> mVibrateFuture = CompletableFuture.completedFuture(null);
 
+    private TorchToggler torchToggler;
+
     private InCallTonePlayer mCallWaitingPlayer;
     private RingtoneFactory mRingtoneFactory;
     private AudioManager mAudioManager;
@@ -231,6 +236,7 @@ public class Ringer {
     private boolean mIsVibrating = false;
 
     private Handler mHandler = null;
+	private int torchMode;
 
     /**
      * Use lock different from the Telecom sync because ringing process is asynchronous outside that
@@ -261,6 +267,7 @@ public class Ringer {
         mRingtoneFactory = ringtoneFactory;
         mInCallController = inCallController;
         mVibrationEffectProxy = vibrationEffectProxy;
+		torchToggler = new TorchToggler(context);
         mUseSimplePattern = mContext.getResources().getBoolean(R.bool.use_simple_vibration_pattern);
         mVibrationPattern = Settings.System.getIntForUser(mContext.getContentResolver(),
             Settings.System.RINGTONE_VIBRATION_PATTERN, 0, UserHandle.USER_CURRENT);
@@ -357,6 +364,18 @@ public class Ringer {
                 } else {
                     effect = mDefaultVibrationEffect;
                 }
+				
+				boolean dndMode = !isRingerAudible;
+				torchMode = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.FLASHLIGHT_ON_CALL, 0, UserHandle.USER_CURRENT);
+
+				boolean shouldFlash = (torchMode == 1 && !dndMode) ||
+									  (torchMode == 2 && dndMode)  ||
+                                       torchMode == 3;
+                if (shouldFlash) {
+                    blinkFlashlight();
+                }
+		
                 if (mVolumeShaperConfig == null) {
                     float silencePoint = (float) (RAMPING_RINGER_VIBRATION_DURATION)
                             / (float) (RAMPING_RINGER_VIBRATION_DURATION + RAMPING_RINGER_DURATION);
@@ -423,6 +442,10 @@ public class Ringer {
         return attributes.shouldAcquireAudioFocus();
     }
 
+    private void blinkFlashlight() {
+        torchToggler = new TorchToggler(mContext);
+        torchToggler.execute();
+    }
     private void maybeStartVibration(Call foregroundCall, boolean shouldRingForContact,
         VibrationEffect effect, boolean isVibrationEnabled, boolean isRingerAudible) {
         synchronized (mLock) {
@@ -514,6 +537,7 @@ public class Ringer {
             }
 
             mRingtonePlayer.stop();
+            torchToggler.stop();
 
             // If we haven't started vibrating because we were waiting for the haptics info, cancel
             // it and don't vibrate at all.
@@ -667,7 +691,49 @@ public class Ringer {
             return false;
         }
     }
+	
+    private class TorchToggler extends AsyncTask {
 
+        private boolean shouldStop = false;
+        private CameraManager cameraManager;
+        private int duration = 500;
+        private boolean hasFlash = true;
+        private Context context;
+
+        public TorchToggler(Context ctx) {
+            this.context = ctx;
+            init();
+        }
+
+        private void init() {
+            cameraManager = (CameraManager) context.getSystemService(Context.CAMERA_SERVICE);
+            hasFlash = context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+        }
+
+        void stop() {
+            shouldStop = true;
+        }
+
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            if (hasFlash) {
+                try {
+                    String cameraId = cameraManager.getCameraIdList()[0];
+                    while (!shouldStop) {
+                        cameraManager.setTorchMode(cameraId, true);
+                        Thread.sleep(duration);
+
+                        cameraManager.setTorchMode(cameraId, false);
+                        Thread.sleep(duration);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+    }
+	
     private void updateVibrationPattern() {
         mVibrationPattern = Settings.System.getIntForUser(mContext.getContentResolver(),
             Settings.System.RINGTONE_VIBRATION_PATTERN, 0, UserHandle.USER_CURRENT);
